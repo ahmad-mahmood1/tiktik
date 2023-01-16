54d7d26256a0068eeaf5fca6930471f583b47bee
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2023-01-16 05:59:39 +0500

    reinitalize with vercel tailwind example

---
+++ app/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Index: NextPage = () => {
+  return <h1 className="text-6xl font-bold underline">Hello!</h1>;
+};
+
+export default Index;
