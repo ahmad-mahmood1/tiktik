4eca26e63e99193618e57f2c8a7e29dab0e30f9d
ahmad-mahmood1 <ahmad.mahmood.a90@gmail.com>
2023-01-19 08:09:08 +0500

    simple setup navbar sidebar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
+          <Image className="cursor-pointer" src={Logo} alt="logo" />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
